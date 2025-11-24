08d7ed0279a1f7fc4e1c5bd395e942a4fa311624
azraji <zdine30@gmail.com>
2025-11-24 16:44:53 +0100

    fix disply none

--- app/todos.js
+++ app/todos.js
@@ -9,9 +9,9 @@ export const store = framework.createStore({
 });
 
 export function createTodoApp(state, filter) {
-  const { todos, editingId  } = state;
+  const { todos, editingId } = state;
 
-  
+
   const filteredTodos = todos.filter((todo) => {
     if (filter === 'active') return !todo.completed;
     if (filter === 'completed') return todo.completed;
@@ -19,256 +19,293 @@ export function createTodoApp(state, filter) {
   });
 
   const handleEditInput = (e, todo) => {
-    if (e.key === 'Enter' && e.target.value.trim().length>1) {
+    if (e.key === 'Enter' && e.target.value.trim().length > 1) {
 
-        const newText = e.target.value.trim();
-        if (newText) {
-          store.update({
-            todos: todos.map(t =>
-              t.id === todo.id ? { ...t, text: newText } : t
-            ),
-            editingId: null
-          });
-      
+      const newText = e.target.value.trim();
+      if (newText) {
+        store.update({
+          todos: todos.map(t =>
+            t.id === todo.id ? { ...t, text: newText } : t
+          ),
+          editingId: null
+        });
+
       }
-    } 
+    }
   };
+  console.log(todos);
+
 
   return createElement({
-    tag: 'section',
-    attrs: { class: 'todoapp' },
+    tag: "div",
+    attrs: { class: "container" },
     children: [
       {
-        tag: 'header',
-        attrs: { class: 'header' },
+        tag: 'section',
+        attrs: { class: 'todoapp' },
         children: [
-          { tag: 'h1', children: ['todos'] },
           {
-            tag: 'input',
-            attrs: {
-              class: 'new-todo',
-              placeholder: 'What needs to be done?',
-              autofocus: true
-            },
-            events: {
-              keydown: (e) => {
-                if (e.key === 'Enter' && e.target.value.trim().length>1) {                  
-                  store.update({
-                    todos: [
-                      ...todos,
-                      {
-                        id: Date.now(),
-                        text: e.target.value.trim(),
-                        completed: false
-                      }
-                    ]
-                  });
-                  e.target.value = '';
+            tag: 'header',
+            attrs: { class: 'header' },
+            children: [
+              { tag: 'h1', children: ['todos'] },
+              {
+                tag: 'input',
+                attrs: {
+                  class: 'new-todo',
+                  placeholder: 'What needs to be done?',
+                  autofocus: true
+                },
+                events: {
+                  keydown: (e) => {
+                    if (e.key === 'Enter' && e.target.value.trim().length > 1) {
+                      store.update({
+                        todos: [
+                          ...todos,
+                          {
+                            id: Date.now(),
+                            text: e.target.value.trim(),
+                            completed: false
+                          }
+                        ]
+                      });
+                      e.target.value = '';
+                    }
+                  }
                 }
               }
-            }
-          }
-        ]
-      },
-      {
-        tag: 'section',
-        attrs: {
-          class: 'main',
-          style: { display: todos.length ? 'block' : 'none' }
-        },
-        children: [
+            ]
+          },
           {
-            tag: 'input',
+            tag: 'section',
             attrs: {
-              id: 'toggle-all',
-              class: 'toggle-all',
-              type: 'checkbox',
-              checked: todos.length > 0 && todos.every(t => t.completed)
+              class: 'main',
             },
-            events: {
-              change: (e) => {   
-                             
-                store.update({
-                  todos: todos.map(t => ({ ...t, completed: e.target.checked }))
-                });
-              }
-            }
-          },
-          {
-            tag: 'label',
-            attrs: { htmlFor: 'toggle-all' },
-            children: ['Mark all as complete']
-          },
-          {
-            tag: 'ul',
-            attrs: { class: 'todo-list' },
-            children: filteredTodos.map(todo => {
+            children: [
 
-              const isEditing = todo.id === editingId;
-
-              let liChildren;
-
-              if (isEditing) {
-                liChildren = [
+             ... (todos.length > 0
+                ? [
                   {
                     tag: 'input',
                     attrs: {
-                      class: 'edit',
-                      value: todo.text,
-                      autofocus: true,
+                      id: 'toggle-all',
+                      class: 'toggle-all',
+                      type: 'checkbox',
+                      checked: todos.length > 0 && todos.every(t => t.completed)
                     },
                     events: {
-                      mount: (el) => {
-                        el.focus();
-                      },
-                      keydown: (e) => handleEditInput(e, todo),
+                      change: (e) => {
+                        store.update({
+                          todos: todos.map(t => ({ ...t, completed: e.target.checked }))
+                        });
+                      }
                     }
-                  }
-                ];      
-
-              } else {
-                liChildren = [
+                  },
                   {
-                    tag: 'div',
-                    attrs: { class: 'view' },
-                    children: [
+                    tag: 'label',
+                    attrs: { htmlFor: 'toggle-all' },
+                    children: ['Mark all as complete']
+                  }
+                ]
+                : []),
+
+              {
+                tag: 'ul',
+                attrs: { class: 'todo-list' },
+                children: filteredTodos.map(todo => {
+
+                  const isEditing = todo.id === editingId;
+
+                  let liChildren;
+
+                  if (isEditing) {
+                    liChildren = [
                       {
                         tag: 'input',
                         attrs: {
-                          class: 'toggle',
-                          type: 'checkbox',
-                          checked: todo.completed
+                          class: 'edit',
+                          value: todo.text,
+                          autofocus: true,
                         },
                         events: {
-                          change: () => {                            
-                            store.update({
-                              todos: todos.map(t =>
-                                t.id === todo.id ? { ...t, completed: !t.completed } : t
-                              )
-                            });
-                          }
+                          mount: (el) => {
+                            el.focus();
+                          },
+                          keydown: (e) => handleEditInput(e, todo),
                         }
-                      },
+                      }
+                    ];
+
+                  } else {
+                    liChildren = [
                       {
-                        tag: 'label',
-                        children: [todo.text || ''],
-                        events: {
-                          dblclick: () => {
-                            store.update({ editingId: todo.id });
+                        tag: 'div',
+                        attrs: { class: 'view' },
+                        children: [
+                          {
+                            tag: 'input',
+                            attrs: {
+                              class: 'toggle',
+                              type: 'checkbox',
+                              checked: todo.completed
+                            },
+                            events: {
+                              change: () => {
+                                store.update({
+                                  todos: todos.map(t =>
+                                    t.id === todo.id ? { ...t, completed: !t.completed } : t
+                                  )
+                                });
+                              }
+                            }
+                          },
+                          {
+                            tag: 'label',
+                            children: [todo.text || ''],
+                            events: {
+                              dblclick: () => {
+                                store.update({ editingId: todo.id });
+                              }
+                            }
+                          },
+                          {
+                            tag: 'button',
+                            attrs: { class: 'destroy' },
+                            events: {
+                              click: () => {
+                                store.update({
+                                  todos: todos.filter(t => t.id !== todo.id)
+                                });
+                              }
+                            }
                           }
-                        }
-                      },
+                        ]
+                      }
+                    ];
+                  }
+
+                  return createElement({
+                    tag: 'li',
+                    attrs: {
+                      class: `${todo.completed ? 'completed' : ''} ${isEditing ? 'editing' : ''}`,
+                      'data-id': todo.id,
+                      key: todo.id
+                    },
+                    children: liChildren
+                  });
+                })
+              }
+            ]
+          },
+
+          (filteredTodos.length > 0) && ({
+            tag: 'footer',
+            attrs: {
+              class: 'footer',
+            },
+            children: [
+              {
+                tag: 'span',
+                attrs: { class: 'todo-count' },
+                children: [
+                  `${todos.filter(t => !t.completed).length} item${todos.filter(t => !t.completed).length !== 1 ? 's' : ''} left`
+                ]
+              },
+              {
+                tag: 'ul',
+                attrs: { class: 'filters' },
+                children: [
+                  {
+                    tag: 'li',
+                    children: [
                       {
-                        tag: 'button',
-                        attrs: { class: 'destroy' },
+                        tag: 'a',
+                        attrs: {
+                          //  href: '/all',
+                          class: filter === 'all' ? 'selected' : ''
+                        },
                         events: {
-                          click: () => {
-                            store.update({
-                              todos: todos.filter(t => t.id !== todo.id)
-                            });
-                          }
-                        }
+                          click: (e) => framework.router.link(e, '')
+                        },
+                        children: ['All']
+                      }
+                    ]
+                  },
+                  {
+                    tag: 'li',
+                    children: [
+                      {
+                        tag: 'a',
+                        attrs: {
+                          //   href: '/active',
+                          class: filter === 'active' ? 'selected' : ''
+                        },
+                        events: {
+                          click: (e) => framework.router.link(e, 'active')
+                        },
+                        children: ['Active']
+                      }
+                    ]
+                  },
+                  {
+                    tag: 'li',
+                    children: [
+                      {
+                        tag: 'a',
+                        attrs: {
+                          class: filter === 'completed' ? 'selected' : ''
+                        },
+                        events: {
+                          click: (e) => framework.router.link(e, 'completed')
+                        },
+                        children: ['Completed']
                       }
                     ]
                   }
-                ];
-              }
-
-              return createElement({
-                tag: 'li',
+                ]
+              },
+              {
+                tag: 'button',
                 attrs: {
-                  class: `${todo.completed ? 'completed' : ''} ${isEditing ? 'editing' : ''}`,
-                  'data-id': todo.id,
-                   key: todo.id
+                  class: 'clear-completed',
                 },
-                children: liChildren
-              });
-            })
-          }
+                children: ['Clear completed'],
+                events: {
+                  click: () => {
+                    store.update({ todos: todos.filter(t => !t.completed) });
+                  }
+                }
+              }
+            ]
+          })
         ]
       },
       {
-        tag: 'footer',
-        attrs: {
-          class: 'footer',
-          style: { display: todos.length ? 'block' : 'none' }
-        },
+        tag: "footer",
+        attrs: { class: "info" },
         children: [
           {
-            tag: 'span',
-            attrs: { class: 'todo-count' },
-            children: [
-              `${todos.filter(t => !t.completed).length} item${todos.filter(t => !t.completed).length !== 1 ? 's' : ''} left`
-            ]
+            tag: "p",
+            children: ["Double-click to edit a todo"]
           },
           {
-            tag: 'ul',
-            attrs: { class: 'filters' },
-            children: [
-              {
-                tag: 'li',
-                children: [
-                  {
-                    tag: 'a',
-                    attrs: {
-                      //  href: '/all',
-                      class: filter === 'all' ? 'selected' : ''
-                    },
-                    events: {
-                      click: (e) => framework.router.link(e, '')
-                    },
-                    children: ['All']
-                  }
-                ]
-              },
-              {
-                tag: 'li',
-                children: [
-                  {
-                    tag: 'a',
-                    attrs: {
-                      //   href: '/active',
-                      class: filter === 'active' ? 'selected' : ''
-                    },
-                    events: {
-                      click: (e) => framework.router.link(e, 'active')
-                    },
-                    children: ['Active']
-                  }
-                ]
-              },
-              {
-                tag: 'li',
-                children: [
-                  {
-                    tag: 'a',
-                    attrs: {
-                      class: filter === 'completed' ? 'selected' : ''
-                    },
-                    events: {
-                      click: (e) => framework.router.link(e, 'completed')
-                    },
-                    children: ['Completed']
-                  }
-                ]
-              }
-            ]
+            tag: "p",
+            children: ["Created by @azraji @ychatoua @abalouri Team"]
           },
           {
-            tag: 'button',
-            attrs: {
-              class: 'clear-completed',
-              style: { display: todos.some(t => t.completed) ? 'block' : 'none' }
-            },
-            children: ['Clear completed'],
-            events: {
-              click: () => {
-                store.update({ todos: todos.filter(t => !t.completed) });
+            tag: "p",
+            children: [
+              "Part of ",
+              {
+                tag: "a",
+                props: { href: "https://learn.zone01oujda.ma/intra/oujda/profile?event=41" },
+                children: ["zone01"]
               }
-            }
+            ]
           }
         ]
       }
+
     ]
   });
 }
